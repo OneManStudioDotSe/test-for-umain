8402f3b5df49429586585606d185bebb8f9eac55
Sotiris Falieris <sotiris@onemanstudio.se>
2024-03-06 13:44:54 +0100

    Add basic DI module for network layer

---
+++ app/src/main/java/se/onemanstudio/test/umain/di/NetworkModule.kt
@@ -0,0 +1,58 @@
+package se.onemanstudio.test.umain.di
+
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import se.onemanstudio.test.umain.BuildConfig
+import se.onemanstudio.test.umain.network.FoodDeliveryService
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+private const val TIMEOUT_60 = 60L
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(TIMEOUT_60, TimeUnit.SECONDS)
+            .readTimeout(TIMEOUT_60, TimeUnit.SECONDS)
+            .writeTimeout(TIMEOUT_60, TimeUnit.SECONDS)
+            .callTimeout(TIMEOUT_60, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideConverterFactory(): GsonConverterFactory =
+        GsonConverterFactory.create(
+            GsonBuilder()
+                .setLenient()
+                .disableHtmlEscaping()
+                .create()
+        )
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient, gsonConverterFactory: GsonConverterFactory): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideDeliveryService(retrofit: Retrofit): FoodDeliveryService = retrofit.create(FoodDeliveryService::class.java)
+}
